66a8749fcc4c2d221c2ca0caa8ba2593e817ca32
kikluz <savir81@hotmail.com>
2016-10-27 14:48:46 -0700

    add controller sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # resources
   resources :customer, only: [:index, :show]
   resources :credits
